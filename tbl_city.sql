edcc1639cb4d7dfcf0fd8c89ca11e50ff45493c7
quanglong <tql0928159331@gmail.com>
2022-05-30 11:19:52 +0700

    Databse MySQL

---
+++ tbl_city.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `tbl_city` (
+  `matp` varchar(5) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(30) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`matp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- ----------------------------
+-- Records of tbl_city
+-- ----------------------------
+INSERT INTO `tbl_city` VALUES ('01', 'Thành phố Hà Nội', 'Thành phố Trung ương');
+INSERT INTO `tbl_city` VALUES ('02', 'Tỉnh Hà Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('04', 'Tỉnh Cao Bằng', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('06', 'Tỉnh Bắc Kạn', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('08', 'Tỉnh Tuyên Quang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('10', 'Tỉnh Lào Cai', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('11', 'Tỉnh Điện Biên', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('12', 'Tỉnh Lai Châu', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('14', 'Tỉnh Sơn La', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('15', 'Tỉnh Yên Bái', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('17', 'Tỉnh Hoà Bình', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('19', 'Tỉnh Thái Nguyên', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('20', 'Tỉnh Lạng Sơn', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('22', 'Tỉnh Quảng Ninh', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('24', 'Tỉnh Bắc Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('25', 'Tỉnh Phú Thọ', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('26', 'Tỉnh Vĩnh Phúc', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('27', 'Tỉnh Bắc Ninh', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('30', 'Tỉnh Hải Dương', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('31', 'Thành phố Hải Phòng', 'Thành phố Trung ương');
+INSERT INTO `tbl_city` VALUES ('33', 'Tỉnh Hưng Yên', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('34', 'Tỉnh Thái Bình', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('35', 'Tỉnh Hà Nam', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('36', 'Tỉnh Nam Định', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('37', 'Tỉnh Ninh Bình', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('38', 'Tỉnh Thanh Hóa', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('40', 'Tỉnh Nghệ An', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('42', 'Tỉnh Hà Tĩnh', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('44', 'Tỉnh Quảng Bình', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('45', 'Tỉnh Quảng Trị', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('46', 'Tỉnh Thừa Thiên Huế', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('48', 'Thành phố Đà Nẵng', 'Thành phố Trung ương');
+INSERT INTO `tbl_city` VALUES ('49', 'Tỉnh Quảng Nam', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('51', 'Tỉnh Quảng Ngãi', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('52', 'Tỉnh Bình Định', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('54', 'Tỉnh Phú Yên', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('56', 'Tỉnh Khánh Hòa', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('58', 'Tỉnh Ninh Thuận', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('60', 'Tỉnh Bình Thuận', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('62', 'Tỉnh Kon Tum', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('64', 'Tỉnh Gia Lai', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('66', 'Tỉnh Đắk Lắk', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('67', 'Tỉnh Đắk Nông', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('68', 'Tỉnh Lâm Đồng', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('70', 'Tỉnh Bình Phước', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('72', 'Tỉnh Tây Ninh', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('74', 'Tỉnh Bình Dương', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('75', 'Tỉnh Đồng Nai', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('77', 'Tỉnh Bà Rịa - Vũng Tàu', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('79', 'Thành phố Hồ Chí Minh', 'Thành phố Trung ương');
+INSERT INTO `tbl_city` VALUES ('80', 'Tỉnh Long An', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('82', 'Tỉnh Tiền Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('83', 'Tỉnh Bến Tre', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('84', 'Tỉnh Trà Vinh', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('86', 'Tỉnh Vĩnh Long', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('87', 'Tỉnh Đồng Tháp', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('89', 'Tỉnh An Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('91', 'Tỉnh Kiên Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('92', 'Thành phố Cần Thơ', 'Thành phố Trung ương');
+INSERT INTO `tbl_city` VALUES ('93', 'Tỉnh Hậu Giang', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('94', 'Tỉnh Sóc Trăng', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('95', 'Tỉnh Bạc Liêu', 'Tỉnh');
+INSERT INTO `tbl_city` VALUES ('96', 'Tỉnh Cà Mau', 'Tỉnh');
